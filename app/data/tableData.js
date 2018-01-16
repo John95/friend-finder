a721db6dbc6153071799d9bc5e946a9f3e922a9b
unknown <jmcarroll95@gmail.com>
2018-01-16 09:59:11 -0500

    htmlRoutes.js is now functions so that you can navigate between web pages.

---
+++ app/data/tableData.js
@@ -0,0 +1,11 @@
+var tableArray = [
+  {
+    customerName: "Hadem",
+    customerEmail: "Hadem@example.com",
+    customerID: "hadem89",
+    phoneNumber: "000-000-0001"
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
